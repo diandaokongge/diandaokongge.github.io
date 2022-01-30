37cfba7fdee794ec50af5018d5b02d65d047b201
diandaokongge <94299076+diandaokongge@users.noreply.github.com>
2022-01-30 14:26:44 +0800

    Update 弱智病毒.bat

--- 别点开/现在关闭还来得及/请不要好奇打开我/47毒/里面的一个程序会使你的电脑的一些程序疯狂弹出，包括任务管理器/确定要打开吗/里面的不是病毒，但很可怕/里面有一个恐怖的bat/病毒.bat/我做了很多文件夹/请不要打开，谢谢/世界，再见/我已经劝说不了你了/弱智病毒.bat
+++ 别点开/现在关闭还来得及/请不要好奇打开我/47毒/里面的一个程序会使你的电脑的一些程序疯狂弹出，包括任务管理器/确定要打开吗/里面的不是病毒，但很可怕/里面有一个恐怖的bat/病毒.bat/我做了很多文件夹/请不要打开，谢谢/世界，再见/我已经劝说不了你了/弱智病毒.bat
@@ -33,18 +33,4 @@ regedit
 SnippingTool
 SecurityHealthSystray
 mobsync
-ping baidu.com
-start C:\危险
-start C:\Users\admin\AppData\Local\Google\Chrome\Application\chrome.exe
-start C:\Program Files\Adobe\Adobe Photoshop 2022\Photoshop.exe
-start C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe
-start C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Excel.lnk
-start C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk
-start C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OneNote.lnk
-start C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Outlook.lnk
-start C:\ProgramData\Microsoft\Windows\Start Menu\Programs\PowerPoint.lnk
-start C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Publisher.lnk
-start C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Word.lnk
-start C:\Program Files (x86)\Tencent\WeChat\WeChat.exe
-start D:\START.exe
 %0
